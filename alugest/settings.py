c5faa86e33911f9a2b691971b5f6669b550faaff
AdriRodri13 <152196347+AdriRodri13@users.noreply.github.com>
2025-06-24 00:46:21 +0200

    Alugest v1.f

--- alugest/settings.py
+++ alugest/settings.py
@@ -8,6 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'insegura-por-defecto')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 
 # Aplicaciones
 INSTALLED_APPS = [
